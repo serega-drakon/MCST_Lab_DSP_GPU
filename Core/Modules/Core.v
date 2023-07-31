4abc8fdcaea5c05a679bc43c3e4a29aedda1d2a3
serega-drakon <megaserega228@gmail.com>
2023-07-31 03:22:48 +0300

    ошметок ядра

--- Core/Modules/Core.v
+++ Core/Modules/Core.v
@@ -1,6 +1,8 @@
 `include "Instruction Set.vh"
 
-module Core (
+module Core #(
+    parameter CORE_NUM = 0
+)(
     input wire clk,
     input wire reset,
 
@@ -52,69 +54,134 @@ module Core (
     reg block; // при включенном block: IP <= IP, fd_insn <= fd_insn, dx_insn <= nop
 
     wire stall;
-    assign stall = block;
+    assign stall = block; //FIXME
 
-    wire branch_cond;
+    wire branch_cond; //FIXME
+    assign branch_cond = 0;
 
-    wire [INSN_OPS_SIZE - 1 : 0] FD_insn_ops;  //для удобства
-    wire [INSN_OPS_SIZE - 1 : 0] DX_insn_ops;
-    wire [INSN_OPS_SIZE - 1 : 0] XM_insn_ops;
-    wire [INSN_OPS_SIZE - 1 : 0] MW_insn_ops;
+    //FIXME: memory part
 
-    wire [INSN_SRC_0_SIZE - 1 : 0] FD_insn_src_0;
-    wire [INSN_SRC_0_SIZE - 1 : 0] DX_insn_src_0;
-    wire [INSN_SRC_0_SIZE - 1 : 0] XM_insn_src_0;
-    wire [INSN_SRC_0_SIZE - 1 : 0] MW_insn_src_0;
+    function [INSN_OPS_SIZE - 1 : 0] insn_ops;
+        input [INSN_SIZE - 1 : 0] insn_reg;
+        begin
+            insn_ops = insn_reg[INSN_OPS_SIZE + INSN_OPS_OFFSET : INSN_OPS_OFFSET];
+        end
+    endfunction
 
-    wire [INSN_SRC_1_SIZE - 1 : 0] FD_insn_src_1;
-    wire [INSN_SRC_1_SIZE - 1 : 0] DX_insn_src_1;
-    wire [INSN_SRC_1_SIZE - 1 : 0] XM_insn_src_1;
-    wire [INSN_SRC_1_SIZE - 1 : 0] MW_insn_src_1;
+    function [INSN_OPS_SIZE - 1 : 0] insn_src_0;
+        input [INSN_SIZE - 1 : 0] insn_reg;
+        begin
+            insn_src_0 = insn_reg[INSN_SRC_0_SIZE + INSN_SRC_0_OFFSET : INSN_SRC_0_OFFSET];
+        end
+    endfunction
 
-    wire [INSN_SRC_2_SIZE - 1 : 0] FD_insn_src_2;
-    wire [INSN_SRC_2_SIZE - 1 : 0] DX_insn_src_2;
-    wire [INSN_SRC_2_SIZE - 1 : 0] XM_insn_src_2;
-    wire [INSN_SRC_2_SIZE - 1 : 0] MW_insn_src_2;
+    function [INSN_SRC_1_SIZE - 1 : 0] insn_src_1;
+        input [INSN_SIZE - 1 : 0] insn_reg;
+        begin
+            insn_src_1 = insn_reg[INSN_SRC_1_SIZE + INSN_SRC_1_OFFSET : INSN_SRC_1_OFFSET];
+        end
+    endfunction
 
-    wire [INSN_DST_SIZE - 1 : 0] FD_insn_dst;
-    wire [INSN_DST_SIZE - 1 : 0] DX_insn_dst;
-    wire [INSN_DST_SIZE - 1 : 0] XM_insn_dst;
-    wire [INSN_DST_SIZE - 1 : 0] MW_insn_dst;
+    function [INSN_SRC_2_SIZE - 1 : 0] insn_src_2;
+        input [INSN_SIZE - 1 : 0] insn_reg;
+        begin
+            insn_src_2 = insn_reg[INSN_SRC_2_SIZE + INSN_SRC_2_OFFSET : INSN_SRC_2_OFFSET];
+        end
+    endfunction
 
-    wire [INSN_CONST_SIZE - 1 : 0] FD_insn_const;  // тупо семиэтажку возвел
-    wire [INSN_CONST_SIZE - 1 : 0] DX_insn_const;
-    wire [INSN_CONST_SIZE - 1 : 0] XM_insn_const;
-    wire [INSN_CONST_SIZE - 1 : 0] MW_insn_const;
+    function [INSN_DST_SIZE - 1 : 0] insn_dst;
+        input [INSN_SIZE - 1 : 0] insn_reg;
+        begin
+            insn_dst = insn_reg[INSN_DST_SIZE + INSN_DST_OFFSET : INSN_DST_OFFSET];
+        end
+    endfunction
 
-    assign FD_insn_ops = FD_insn_reg[INSN_OPS_SIZE + INSN_OPS_OFFSET : INSN_OPS_OFFSET];
-    assign DX_insn_ops = DX_insn_reg[INSN_OPS_SIZE + INSN_OPS_OFFSET : INSN_OPS_OFFSET];
-    assign XM_insn_ops = XM_insn_reg[INSN_OPS_SIZE + INSN_OPS_OFFSET : INSN_OPS_OFFSET];
-    assign MW_insn_ops = MW_insn_reg[INSN_OPS_SIZE + INSN_OPS_OFFSET : INSN_OPS_OFFSET];
+    function [INSN_CONST_SIZE - 1 : 0] insn_const;
+        input [INSN_SIZE - 1 : 0] insn_reg;
+        begin
+            insn_const = insn_reg[INSN_CONST_SIZE + INSN_CONST_OFFSET : INSN_CONST_OFFSET];
+        end
+    endfunction
 
-    assign FD_insn_src_0 = FD_insn_reg[INSN_SRC_0_SIZE + INSN_SRC_0_OFFSET : INSN_SRC_0_OFFSET];
-    assign DX_insn_src_0 = DX_insn_reg[INSN_SRC_0_SIZE + INSN_SRC_0_OFFSET : INSN_SRC_0_OFFSET];
-    assign XM_insn_src_0 = XM_insn_reg[INSN_SRC_0_SIZE + INSN_SRC_0_OFFSET : INSN_SRC_0_OFFSET];
-    assign MW_insn_src_0 = MW_insn_reg[INSN_SRC_0_SIZE + INSN_SRC_0_OFFSET : INSN_SRC_0_OFFSET];
+    wire [INSN_OPS_SIZE - 1 : 0] FD_insn_ops = insn_ops(FD_insn_reg);  //для удобства
+    wire [INSN_OPS_SIZE - 1 : 0] DX_insn_ops = insn_ops(DX_insn_reg);
+    wire [INSN_OPS_SIZE - 1 : 0] XM_insn_ops = insn_ops(XM_insn_reg);
+    wire [INSN_OPS_SIZE - 1 : 0] MW_insn_ops = insn_ops(MW_insn_reg);
 
-    assign MW_insn_src_1 = FD_insn_reg[INSN_SRC_1_SIZE + INSN_SRC_1_OFFSET : INSN_SRC_1_OFFSET];
-    assign MW_insn_src_1 = DX_insn_reg[INSN_SRC_1_SIZE + INSN_SRC_1_OFFSET : INSN_SRC_1_OFFSET];
-    assign MW_insn_src_1 = XM_insn_reg[INSN_SRC_1_SIZE + INSN_SRC_1_OFFSET : INSN_SRC_1_OFFSET];
-    assign MW_insn_src_1 = MW_insn_reg[INSN_SRC_1_SIZE + INSN_SRC_1_OFFSET : INSN_SRC_1_OFFSET];
+    wire [INSN_SRC_0_SIZE - 1 : 0] FD_insn_src_0 = insn_src_0(FD_insn_reg);
+    wire [INSN_SRC_0_SIZE - 1 : 0] DX_insn_src_0 = insn_src_0(DX_insn_reg);
+    wire [INSN_SRC_0_SIZE - 1 : 0] XM_insn_src_0 = insn_src_0(XM_insn_reg);
+    wire [INSN_SRC_0_SIZE - 1 : 0] MW_insn_src_0 = insn_src_0(MW_insn_reg);
 
-    assign MW_insn_src_2 = FD_insn_reg[INSN_SRC_2_SIZE + INSN_SRC_2_OFFSET : INSN_SRC_2_OFFSET];
-    assign MW_insn_src_2 = DX_insn_reg[INSN_SRC_2_SIZE + INSN_SRC_2_OFFSET : INSN_SRC_2_OFFSET];
-    assign MW_insn_src_2 = XM_insn_reg[INSN_SRC_2_SIZE + INSN_SRC_2_OFFSET : INSN_SRC_2_OFFSET];
-    assign MW_insn_src_2 = MW_insn_reg[INSN_SRC_2_SIZE + INSN_SRC_2_OFFSET : INSN_SRC_2_OFFSET];
+    wire [INSN_SRC_1_SIZE - 1 : 0] FD_insn_src_1 = insn_src_1(FD_insn_reg);
+    wire [INSN_SRC_1_SIZE - 1 : 0] DX_insn_src_1 = insn_src_1(DX_insn_reg);
+    wire [INSN_SRC_1_SIZE - 1 : 0] XM_insn_src_1 = insn_src_1(XM_insn_reg);
+    wire [INSN_SRC_1_SIZE - 1 : 0] MW_insn_src_1 = insn_src_1(MW_insn_reg);
 
-    assign FD_insn_dst = FD_insn_reg[INSN_DST_SIZE + INSN_DST_OFFSET : INSN_DST_OFFSET];
-    assign DX_insn_dst = DX_insn_reg[INSN_DST_SIZE + INSN_DST_OFFSET : INSN_DST_OFFSET];
-    assign XM_insn_dst = XM_insn_reg[INSN_DST_SIZE + INSN_DST_OFFSET : INSN_DST_OFFSET];
-    assign MW_insn_dst = MW_insn_reg[INSN_DST_SIZE + INSN_DST_OFFSET : INSN_DST_OFFSET];
+    wire [INSN_SRC_2_SIZE - 1 : 0] FD_insn_src_2 = insn_src_2(FD_insn_reg);
+    wire [INSN_SRC_2_SIZE - 1 : 0] DX_insn_src_2 = insn_src_2(DX_insn_reg);
+    wire [INSN_SRC_2_SIZE - 1 : 0] XM_insn_src_2 = insn_src_2(XM_insn_reg);
+    wire [INSN_SRC_2_SIZE - 1 : 0] MW_insn_src_2 = insn_src_2(MW_insn_reg);
 
-    assign FD_insn_const = FD_insn_reg[INSN_CONST_SIZE + INSN_CONST_OFFSET : INSN_CONST_OFFSET];  // больше многоэтажек!!!!!!!
-    assign DX_insn_const = DX_insn_reg[INSN_CONST_SIZE + INSN_CONST_OFFSET : INSN_CONST_OFFSET];
-    assign XM_insn_const = XM_insn_reg[INSN_CONST_SIZE + INSN_CONST_OFFSET : INSN_CONST_OFFSET];
-    assign MW_insn_const = MW_insn_reg[INSN_CONST_SIZE + INSN_CONST_OFFSET : INSN_CONST_OFFSET];
+    wire [INSN_DST_SIZE - 1 : 0] FD_insn_dst = insn_dst(FD_insn_reg);
+    wire [INSN_DST_SIZE - 1 : 0] DX_insn_dst = insn_dst(DX_insn_reg);
+    wire [INSN_DST_SIZE - 1 : 0] XM_insn_dst = insn_dst(XM_insn_reg);
+    wire [INSN_DST_SIZE - 1 : 0] MW_insn_dst = insn_dst(MW_insn_reg);
+
+    wire [INSN_CONST_SIZE - 1 : 0] FD_insn_const = insn_const(FD_insn_reg);
+    wire [INSN_CONST_SIZE - 1 : 0] DX_insn_const = insn_const(DX_insn_reg);
+    wire [INSN_CONST_SIZE - 1 : 0] XM_insn_const = insn_const(XM_insn_reg);
+    wire [INSN_CONST_SIZE - 1 : 0] MW_insn_const = insn_const(MW_insn_reg);
+
+    function insn_is_F1;
+        input [INSN_OPS_SIZE - 1 : 0] insn_ops;
+        begin
+            insn_is_F1 = (insn_ops == `NOP | insn_ops == `ADD | insn_ops == `SUB | insn_ops == `MUL
+                | insn_ops == `DIV | insn_ops == `CMPGE | insn_ops == `RSHIFT | insn_ops == `LSHIFT
+                | insn_ops == `AND | insn_ops == `OR | insn_ops == `XOR | insn_ops == `LD);
+        end
+    endfunction // это должно синтезироваться в провод
+
+    function insn_is_F2;
+        input [INSN_OPS_SIZE - 1 : 0] insn_ops;
+        begin
+            insn_is_F2 = (insn_ops == `SET_CONST);
+        end
+    endfunction
+
+    function insn_is_F3;
+        input [INSN_OPS_SIZE - 1 : 0] insn_ops;
+        begin
+            insn_is_F3 = (insn_ops == `ST);
+        end
+    endfunction
+
+    function insn_is_F4;
+        input [INSN_OPS_SIZE - 1 : 0] insn_ops;
+        begin
+            insn_is_F4 = (insn_ops == `BNZ | insn_ops == `READY);
+        end
+    endfunction
+
+    wire FD_insn_is_F1 = insn_is_F1(FD_insn_ops);
+    wire DX_insn_is_F1 = insn_is_F1(DX_insn_ops);
+    wire XM_insn_is_F1 = insn_is_F1(XM_insn_ops);
+    wire MW_insn_is_F1 = insn_is_F1(MW_insn_ops);
+
+    wire FD_insn_is_F2 = insn_is_F2(FD_insn_ops);
+    wire DX_insn_is_F2 = insn_is_F2(DX_insn_ops);
+    wire XM_insn_is_F2 = insn_is_F2(XM_insn_ops);
+    wire MW_insn_is_F2 = insn_is_F2(MW_insn_ops);
+
+    wire FD_insn_is_F3 = insn_is_F3(FD_insn_ops);
+    wire DX_insn_is_F3 = insn_is_F3(DX_insn_ops);
+    wire XM_insn_is_F3 = insn_is_F3(XM_insn_ops);
+    wire MW_insn_is_F3 = insn_is_F3(MW_insn_ops);
+
+    wire FD_insn_is_F4 = insn_is_F4(FD_insn_ops);
+    wire DX_insn_is_F4 = insn_is_F4(DX_insn_ops);
+    wire XM_insn_is_F4 = insn_is_F4(XM_insn_ops);
+    wire MW_insn_is_F4 = insn_is_F4(MW_insn_ops);
 
     always @(posedge clk)
         if(reset)
@@ -124,7 +191,7 @@ module Core (
         else
             Ready <= (MW_insn_ops == `READY) ? 1 : Ready;
 
-    always @(posedge clk) //пока не сделаю нормальный модуль stall будет это
+    always @(posedge clk)
         if(reset)
             block <= 1;
         else if(Start & Ready)
@@ -147,6 +214,14 @@ module Core (
             insn_ptr <= 0;
         else
             insn_ptr <= (~stall) ? insn_ptr + 1 : insn_ptr;
+
+    generate for(genvar i = 0; i < REG_COUNT; i = i + 1)
+        always @(posedge clk)
+            if(reset)
+                r[i] <= 0;
+            else
+                r[i] <= 0; //FIXME
+    endgenerate
 
     always @(posedge clk)
         if(~reset)
