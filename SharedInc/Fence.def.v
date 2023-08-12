7f149928368a5b6d0d0979695be1027a878f0a8d
Gosha Bagrov <89145061+GoshaAB29@users.noreply.github.com>
2023-08-12 20:24:49 +0300

    перегруппировал константы, добавил новые (для TS)

---
+++ SharedInc/Fence.def.v
@@ -0,0 +1,9 @@
+`ifndef FENCE
+`define FENCE
+
+`define NO  4'h0		//no
+`define ACQ 4'h1		//acquire
+`define REL 4'h2		//release
+`define RSV 4'h3		//reserve
+
+`endif //FENCE
--- SharedInc/Ranges.def.v
+++ SharedInc/Ranges.def.v
@@ -1,21 +1,26 @@
 `ifndef CONSTANTS
 `define CONSTANTS
 
+`define	NUM_OF_CORES 16
+`define	CORES_RANGE (`NUM_OF_CORES - 1) : (0)
+`define CORE_ID_SIZE 4
+`define CORE_ID_RANGE (`CORE_ID_SIZE - 1) : (0)
+
 `define REG_COUNT 16
 `define REG_PTR_SIZE 4
 `define REG_PTR_RANGE (`REG_PTR_SIZE - 1) : (0)
 `define REG_SIZE 8
 `define REG_RANGE (`REG_SIZE - 1) : (0)
-`define CORE_ID_SIZE 4
-`define CORE_ID_RANGE (`CORE_ID_SIZE - 1) : (0)
+
 `define ADDR_SIZE 12
 `define ADDR_RANGE (`ADDR_SIZE - 1) : (0)
+
+
 `define INSN_COUNT 16
 `define INSN_SIZE 16
 `define INSN_RANGE (`INSN_SIZE - 1) : (0)
 `define INSN_BUS_RANGE (`INSN_COUNT * `INSN_SIZE - 1) : (0)
-// здесь указатель на всю инструкцию, а не конкретный байт, те
-// без нуля в младшем бите
+
 `define INSN_PTR_SIZE 4
 `define INSN_PTR_RANGE (`INSN_PTR_SIZE - 1) : (0)
 `define INSN_OPC_SIZE 4
@@ -46,16 +51,37 @@
 `define INSN_TARGET_OFFSET 4
 `define INSN_TARGET_OFFSET_RANGE (`INSN_TARGET_SIZE + `INSN_TARGET_OFFSET - 1) : (`INSN_TARGET_OFFSET)
 `define INSN_TARGET_RANGE (`INSN_TARGET_SIZE - 1) : (0)
+
+
 `define	BANK_DATA_SIZE 256
 `define	BANK_DATA_RANGE (`BANK_DATA_SIZE - 1) : (0)
-`define	NUM_OF_CORES 16
-`define	CORES_RANGE (`NUM_OF_CORES - 1) : (0)
-`define ADDR_BUS_RANGE (`ADDR_SIZE * `NUM_OF_CORES - 1) : (0)
-`define	REG_BUS_RANGE (`REG_SIZE * `NUM_OF_CORES - 1) : (0)
-`define	ENABLE_BUS_RANGE (2 * `NUM_OF_CORES - 1) : (0)
 `define	NUM_OF_BANKS 16
 `define	BANKS_RANGE (`NUM_OF_BANKS - 1) : (0)
 `define BANK_ID_SIZE 4
 `define BANK_ID_RANGE (`BANK_ID_SIZE - 1) : (0)
+
+`define ADDR_BUS_RANGE (`ADDR_SIZE * `NUM_OF_CORES - 1) : (0)
+`define	REG_BUS_RANGE (`REG_SIZE * `NUM_OF_CORES - 1) : (0)
+`define	ENABLE_BUS_RANGE (2 * `NUM_OF_CORES - 1) : (0)
+
+
+`define TASK_MEM_DEPTH	64
+`define TASK_MEM_WIDTH	256
+
+`define SIZE_IF_NUM	$clog2(`TASK_MEM_DEPTH)
+`define IF_NUM_RANGE	(`SIZE_IF_NUM - 1) : (0)
+`define TS_FENCE_RANGE	(`SIZE_IF_NUM + 1) : (`SIZE_IF_NUM)
+
+
+`define TM_DEPTH_RANGE (`TASK_MEM_DEPTH - 1) : (0)
+`define TM_WIDTH_RANGE (`TASK_MEM_DEPTH - 1) : (0)
+`define TM_RANGE       (`NUM_OF_CORES * `INSN_SIZE * `INSN_COUNT - 1) : (0)
+`define R0_VECT_RANGE (3 * 16 - 1) : (2 * 16)
+`define CORE_ACTIVE_VECT_RANGE (2 * 16 - 1) : (1 * 16)
+
+`define R0_RANGE(jj)	(jj * `REG_SIZE + `REG_SIZE - 1) : (jj * `REG_SIZE)
+`define TM_R0_RANGE(jj)									\
+	(`TASK_MEM_WIDTH - `REG_SIZE * (`NUM_OF_CORES - jj) + `REG_SIZE - 1) : 		\
+	(`TASK_MEM_WIDTH - `REG_SIZE * (`NUM_OF_CORES - jj))
 
 `endif //CONSTANTS
