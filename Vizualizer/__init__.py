5b4dc55fc48ccbb0e68ab795e71229e759bcb695
serega-drakon <megaserega228@gmail.com>
2023-08-13 22:49:44 +0300

    лджд

---
+++ Vizualizer/__init__.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('_mpl-gallery-nogrid')
+
+# make data
+#X, Y = np.meshgrid(np.linspace(-n, n, 16), np.linspace(-n, n, 16))
+#Z = (1 - X/2 + X**5 + Y**3) * np.exp(-X**2 - Y**2)
+
+# plot
+#fig, ax = plt.subplots()
+
+#ax.imshow(Z, cmap='gray', vmin=0, vmax=255)
+
+#ax.imshow(Z)
+#plt.show()
+
