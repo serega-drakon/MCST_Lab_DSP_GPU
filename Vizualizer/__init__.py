475f76b5e9daaca552e25a067f74b54e85dfc9c6
serega-drakon <megaserega228@gmail.com>
2023-08-17 22:19:00 +0300

    jkkjjkkj

--- Vizualizer/__init__.py
+++ Vizualizer/__init__.py
@@ -3,17 +3,23 @@ import numpy as np
 
 plt.style.use('_mpl-gallery-nogrid')
 
+with open('input.txt') as f:
+    array = []
+    for line in f: # read rest of lines
+        array.append([int(x) for x in line.split()])
 
+print(array)
 
+#n = 32
 # make data
-#X, Y = np.meshgrid(np.linspace(-n, n, 16), np.linspace(-n, n, 16))
+#X, Y = np.meshgrid(np.linspace(-n, n, 64), np.linspace(-n, n, 64))
 #Z = (1 - X/2 + X**5 + Y**3) * np.exp(-X**2 - Y**2)
-
+Z = array
 # plot
-#fig, ax = plt.subplots()
+fig, ax = plt.subplots()
 
 #ax.imshow(Z, cmap='gray', vmin=0, vmax=255)
 
-#ax.imshow(Z)
-#plt.show()
+ax.imshow(Z)
+plt.show()
 
