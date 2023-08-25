c994fcc450e1693a95290ce18bab3003c6511336
serega-drakon <megaserega228@gmail.com>
2023-08-26 00:05:07 +0300

    воыововововововоовововов

--- Core/Modules/InsnMemory.v
+++ Core/Modules/InsnMemory.v
@@ -7,6 +7,7 @@ module InsnMemory (
     input wire init_insn_mem, // Start&Ready
     input wire [`INSN_BUS_RANGE] insn_data,
     input wire [`INSN_PTR_RANGE] insn_ptr,
+    input wire [`INSN_LOAD_COUNTER_RANGE] insn_load_counter,
     output wire [`INSN_SIZE - 1 : 0] insn_curr
 );
     reg [`INSN_SIZE - 1 : 0] insn_mem_r[`INSN_COUNT - 1 : 0];
@@ -14,10 +15,13 @@ module InsnMemory (
     assign insn_curr = insn_mem_r[insn_ptr];
 
     generate
-        for(genvar i = 0; i < `INSN_COUNT; i = i + 1) begin : insn_mem_loop
+        for(genvar i = 0; i < `INSN_LOAD_TIME; i = i + 1)
+        for(genvar j = 0; j < `INSN_BUS_COUNT; j = j + 1) begin : insn_mem_loop
             always @(posedge clk)
-                insn_mem_r[i] <= (~reset & init_insn_mem) ?
-                    insn_data[(i + 1) * `INSN_SIZE - 1 : i * `INSN_SIZE] : insn_mem_r[i];
+                insn_mem_r[j + i * `INSN_BUS_COUNT] <=
+                    (~reset & init_insn_mem & insn_load_counter == i) ?
+                        insn_data[(j + 1) * `INSN_SIZE - 1 : j * `INSN_SIZE] :
+                        insn_mem_r[j + i * `INSN_BUS_COUNT];
         end
     endgenerate
 
