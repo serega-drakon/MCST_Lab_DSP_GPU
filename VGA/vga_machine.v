e5b98385f9a631663716a245588d578aae1e648e
serega-drakon <megaserega228@gmail.com>
2024-04-18 16:02:14 +0300

    commit

---
+++ VGA/vga_machine.v
@@ -0,0 +1,43 @@
+`include "vga.v"
+
+
+module vga_machine(
+	input           clk,
+	input           rst,
+	
+	output          vga_clk,
+	output          h_sync,
+	output          v_sync,
+	output          blank_n,
+	output          sync_n,
+	output wire 	[`REG_RANGE]	red_vga,
+	output wire 	[`REG_RANGE]	green_vga,
+	output wire 	[`REG_RANGE]	blue_vga,
+	
+	output wire	[`ADDR_RANGE]	vga_addr,
+	input		[`REG_RANGE]	vga_data
+);
+
+wire	[9:0]	x;
+wire	[9:0]	y;
+
+vga vga_0(
+	.clk(clk),
+	.rst(rst),
+	
+	.vga_clk(vga_clk),
+	.h_sync(h_sync),
+	.v_sync(v_sync),
+	.blank_n(blank_n),
+	.sync_n(sync_n),
+	.point_pos_x(x),
+	.point_pos_y(y)
+);
+
+assign red_vga = vga_data;
+assign green_vga = vga_data;
+assign blue_vga = vga_data;
+
+assign vga_addr = {y[9:5], x[9:3]};
+
+endmodule
---
+++ VGA/vga_sync.v
@@ -0,0 +1,73 @@
+module vga_sync (
+    input   clk,
+    input   rst,
+
+    output  reg     h_sync,
+    output  reg     v_sync,
+    output  [9:0]   pos_x,
+    output  [9:0]   pos_y,
+    output          blank_n
+);
+
+localparam h_front_t = 16;
+localparam h_sync_t = 96;
+localparam h_back_t = 48;
+localparam h_active_t = 640;
+localparam h_blank_t = h_front_t + h_sync_t + h_back_t;
+localparam h_total_t = h_active_t + h_blank_t;
+localparam h_blank_t_div5 = h_blank_t / 5;
+
+localparam v_front_t = 10;
+localparam v_sync_t = 2;
+localparam v_back_t = 33;
+localparam v_active_t = 480;
+localparam v_blank_t = v_front_t + v_sync_t + v_back_t;
+localparam v_total_t = v_active_t + v_blank_t;
+localparam v_blank_t_div15 = v_blank_t / 15;
+
+reg [9:0] h_counter;
+reg [9:0] v_counter;
+
+reg [9:0] h_counter_div5;
+reg [9:0] v_counter_div15;
+
+always @(posedge clk)
+begin
+	if(rst)
+	begin
+		{h_counter_div5, v_counter_div15} <= 20'b0;
+        //h_counter_div5 <= 0;//
+        //v_counter_div15 <= 0;//d
+	end
+    else
+    begin
+	    h_counter_div5 <= h_counter_div5 + ((h_counter_div5 + 1) * 5 == h_counter);
+	    v_counter_div15 <= v_counter_div15 + ((v_counter_div15 + 1) * 15 == v_counter);
+    end
+end
+
+assign pos_x = h_counter_div5 - h_blank_t_div5;
+assign pos_y = v_counter_div15 - v_blank_t_div15;
+
+assign blank_n = ~((h_counter < h_blank_t) || (v_counter < v_blank_t));
+
+always @(posedge clk)
+begin
+    if (rst)
+    begin
+        {h_sync, v_sync, h_counter, v_counter} <= 22'd0;
+    end
+    else
+    begin
+        h_counter <= (h_counter == h_total_t - 1) ? 10'd0 : h_counter + 1'd1;
+        h_sync <= (h_counter < h_front_t - 1) | (h_counter > h_front_t + h_sync_t - 1);
+
+        if (h_counter == h_front_t + h_sync_t - 1)
+        begin
+            v_counter <= (v_counter == v_total_t - 1) ? 10'd0 : v_counter + 1'd1;
+            v_sync <= (v_counter < v_front_t - 1) | (v_counter > v_front_t + v_sync_t - 1);
+        end
+    end
+end
+
+endmodule
