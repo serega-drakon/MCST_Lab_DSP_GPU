4d7ed5b593fca8c9591ee358a4d7ef7ca2e53864
serega-drakon <megaserega228@gmail.com>
2024-05-02 18:00:56 +0300

    exp

--- Scheduler/TS.v
+++ Scheduler/TS.v
@@ -181,18 +181,18 @@ module Task_Scheduler
 		begin
 			if (reset)
 				Task_Pointer <= 0;					        //initially TM is empty or old
-				
-			else if( (vga_stop) | 
-					 (Insn_Frame_Num == 0 & INSN_FRAME_NUM_NEXT == 0 
-					 & STOP_NEXT & EXEC_MASK == 0 ) )
-				Task_Pointer <= STOP_ADDR_NEXT;             //maybe Task_Pointer;
+			else if(vga_stop)
+				Task_Pointer <= Task_Pointer;
+			else if(Insn_Frame_Num == 0 & INSN_FRAME_NUM_NEXT == 0
+					 & STOP_NEXT & EXEC_MASK == 0 )
+				Task_Pointer <= STOP_ADDR_NEXT;             //FIXME: maybe Task_Pointer;
 				
 			else if(Insn_Frame_Num == 0 &
 				   (EXEC_MASK == 0 & (exec_block_cond |
 					insn_free_no_fence) ))
 				Task_Pointer <= Task_Pointer + 1;
 				
-			else if(Insn_Frame_Num == 1 & FLAG_TIME)
+			else if(Insn_Frame_Num == 1 & FLAG_TIME) //TODO: TEST!!!!!!!!!
 				Task_Pointer <= (stop_r) ? stop_addr_r : Task_Pointer + 1;
 				
 			else if(Insn_Frame_Num > 1 & FLAG_TIME & insn_finish)
